3a93e77445931b244ad37441579f33ba45a3a450
isDohyeon <eun041212@gmail.com>
2025-08-13 17:31:55 +0900

    Binary_Tree/Q3 : countOneChildNodes 구현

--- Data-Structures/Binary_Tree/Q3_E_BT.c
+++ Data-Structures/Binary_Tree/Q3_E_BT.c
@@ -10,23 +10,20 @@ Purpose: Implementing the required functions for Question 3 */
 #include <stdlib.h>
 
 //////////////////////////////////////////////////////////////////////////////////
-typedef struct _btnode
-{
+typedef struct _btnode {
     int item;
     struct _btnode *left;
     struct _btnode *right;
-} BTNode;   // You should not change the definition of BTNode
+} BTNode; // You should not change the definition of BTNode
 
 /////////////////////////////////////////////////////////////////////////////////
 
-typedef struct _stackNode
-{
+typedef struct _stackNode {
     BTNode *btnode;
     struct _stackNode *next;
 } StackNode;
 
-typedef struct _stack
-{
+typedef struct _stack {
     StackNode *top;
 } Stack;
 
@@ -38,35 +35,30 @@ int countOneChildNodes(BTNode *node);
 BTNode *createBTNode(int item);
 
 BTNode *createTree();
-void push( Stack *stack, BTNode *node);
-BTNode* pop(Stack *stack);
+void push(Stack *stack, BTNode *node);
+BTNode *pop(Stack *stack);
 
 void printTree(BTNode *node);
 void removeAll(BTNode **node);
 
 ///////////////////////////// main() /////////////////////////////////////////////
 
-int main()
-{
+int main() {
     char e;
-    int c,s;
+    int c, s;
     BTNode *root;
 
     c = 1;
     root = NULL;
 
-
     printf("1: Create a binary tree.\n");
     printf("2: Count the number of nodes that have exactly one child node.\n");
     printf("0: Quit;\n");
 
-    while(c != 0)
-    {
+    while (c != 0) {
         printf("Please input your choice(1/2/0): ");
-        if( scanf("%d",&c) > 0)
-        {
-            switch(c)
-            {
+        if (scanf("%d", &c) > 0) {
+            switch (c) {
             case 1:
                 removeAll(&root);
                 root = createTree();
@@ -86,28 +78,30 @@ int main()
                 printf("Choice unknown;\n");
                 break;
             }
+        } else {
+            scanf("%c", &e);
         }
-        else
-        {
-            scanf("%c",&e);
-        }
-
     }
     return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 
-int countOneChildNodes(BTNode *node)
+int countOneChildNodes(BTNode *node) {
+    if (node == NULL) {
+        return 0;
+    }
 
-{
-    /* add your code here */
+    int isOneChild = (node->left == NULL && node->right != NULL) || (node->left != NULL && node->right == NULL);
+    int leftCount = countOneChildNodes(node->left);
+    int rightCount = countOneChildNodes(node->right);
+
+    return leftCount + rightCount + (isOneChild ? 1 : 0);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-BTNode *createBTNode(int item)
-{
+BTNode *createBTNode(int item) {
     BTNode *newNode = malloc(sizeof(BTNode));
     newNode->item = item;
     newNode->left = NULL;
@@ -117,9 +111,7 @@ BTNode *createBTNode(int item)
 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-BTNode *createTree()
-{
+BTNode *createTree() {
     Stack stack;
     BTNode *root, *temp;
     char s;
@@ -129,77 +121,61 @@ BTNode *createTree()
     root = NULL;
     printf("Input an integer that you want to add to the binary tree. Any Alpha value will be treated as NULL.\n");
     printf("Enter an integer value for the root: ");
-    if(scanf("%d",&item) > 0)
-    {
+    if (scanf("%d", &item) > 0) {
         root = createBTNode(item);
-        push(&stack,root);
-    }
-    else
-    {
-        scanf("%c",&s);
+        push(&stack, root);
+    } else {
+        scanf("%c", &s);
     }
 
-    while((temp =pop(&stack)) != NULL)
-    {
+    while ((temp = pop(&stack)) != NULL) {
 
         printf("Enter an integer value for the Left child of %d: ", temp->item);
 
-        if(scanf("%d",&item)> 0)
-        {
+        if (scanf("%d", &item) > 0) {
             temp->left = createBTNode(item);
-        }
-        else
-        {
-            scanf("%c",&s);
+        } else {
+            scanf("%c", &s);
         }
 
         printf("Enter an integer value for the Right child of %d: ", temp->item);
-        if(scanf("%d",&item)>0)
-        {
+        if (scanf("%d", &item) > 0) {
             temp->right = createBTNode(item);
-        }
-        else
-        {
-            scanf("%c",&s);
+        } else {
+            scanf("%c", &s);
         }
 
-        if(temp->right != NULL)
-            push(&stack,temp->right);
-        if(temp->left != NULL)
-            push(&stack,temp->left);
+        if (temp->right != NULL)
+            push(&stack, temp->right);
+        if (temp->left != NULL)
+            push(&stack, temp->left);
     }
     return root;
 }
 
-void push( Stack *stack, BTNode *node)
-{
+void push(Stack *stack, BTNode *node) {
     StackNode *temp;
 
     temp = malloc(sizeof(StackNode));
-    if(temp == NULL)
+    if (temp == NULL)
         return;
     temp->btnode = node;
-    if(stack->top == NULL)
-    {
+    if (stack->top == NULL) {
         stack->top = temp;
         temp->next = NULL;
-    }
-    else
-    {
+    } else {
         temp->next = stack->top;
         stack->top = temp;
     }
 }
 
-BTNode* pop(Stack *stack)
-{
+BTNode *pop(Stack *stack) {
     StackNode *temp, *top;
     BTNode *ptr;
     ptr = NULL;
 
     top = stack->top;
-    if(top != NULL)
-    {
+    if (top != NULL) {
         temp = top->next;
         ptr = top->btnode;
 
@@ -210,23 +186,20 @@ BTNode* pop(Stack *stack)
     return ptr;
 }
 
-void printTree(BTNode *node)
-{
-    if(node == NULL) return;
+void printTree(BTNode *node) {
+    if (node == NULL)
+        return;
 
     printTree(node->left);
-    printf("%d ",node->item);
+    printf("%d ", node->item);
     printTree(node->right);
 }
 
-void removeAll(BTNode **node)
-{
-    if(*node != NULL)
-    {
+void removeAll(BTNode **node) {
+    if (*node != NULL) {
         removeAll(&((*node)->left));
         removeAll(&((*node)->right));
         free(*node);
         *node = NULL;
     }
 }
-
